e353edb04fe4fb3e7e857f61a8f3636845a68eb4
Chen Jiajun <c.jiajun.92@gmail.com>
2025-10-24 21:52:43 +0800

    add stage for mvn test

---
+++ src/test/java/sg/com/chen/Beginner349Application/Beginner349ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.com.chen.Beginner349Application;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Beginner349ApplicationTests {
+    @Test
+    void contextLoads() {
+        Assertions.assertTrue(true, "This assertion should always be true.");
+    }
+}
