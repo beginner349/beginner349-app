f5f47fb154d9aafaa7346fd5ca31684d643e9f3e
Chen Jiajun <c.jiajun.92@gmail.com>
2025-10-30 21:32:15 +0800

    fix unit test

--- src/test/java/sg/com/chen/Beginner349Application/Beginner349ApplicationTests.java
+++ src/test/java/sg/com/chen/Beginner349Application/Beginner349ApplicationTests.java
@@ -3,9 +3,14 @@ package sg.com.chen.Beginner349Application;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 @SpringBootTest
 class Beginner349ApplicationTests {
+    @MockitoBean
+    private DynamoDbClient dynamoDbClient;
+
     @Test
     void contextLoads() {
         Assertions.assertTrue(true, "This assertion should always be true.");
