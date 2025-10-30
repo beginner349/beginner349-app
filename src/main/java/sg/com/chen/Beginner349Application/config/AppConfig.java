8689b73bce2808015f0e53865a2016d581339d1c
Chen Jiajun <c.jiajun.92@gmail.com>
2025-10-30 21:19:33 +0800

    add code for dynamodb

--- src/main/java/sg/com/chen/Beginner349Application/config/AppConfig.java
+++ src/main/java/sg/com/chen/Beginner349Application/config/AppConfig.java
@@ -1,8 +1,13 @@
 package sg.com.chen.Beginner349Application.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 @Configuration
 public class AppConfig {
-
+    @Bean
+    public DynamoDbClient dynamoDbClient() {
+        return DynamoDbClient.create();
+    }
 }
---
+++ src/main/java/sg/com/chen/Beginner349Application/controller/DynamodbController.java
@@ -0,0 +1,25 @@
+package sg.com.chen.Beginner349Application.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.ListTablesResponse;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/dynamodb")
+public class DynamodbController {
+    private final DynamoDbClient dynamoDbClient;
+
+    public DynamodbController(DynamoDbClient dynamoDbClient) {
+        this.dynamoDbClient = dynamoDbClient;
+    }
+
+    @GetMapping("/list")
+    public List<String> listTable() {
+        ListTablesResponse listTablesResponse = dynamoDbClient.listTables();
+        return listTablesResponse.tableNames();
+    }
+}
